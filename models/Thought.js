3c01962e5f1719392c254e9f61e84a649a581fb9
Kate Spitalnic <kespitalnic@gmail.com>
2022-03-24 22:58:47 -0400

    thought model virtual

--- models/Thought.js
+++ models/Thought.js
@@ -4,21 +4,46 @@ const ThoughtSchmea = new Schema({
     thoughtText:{
         type: String,
         required: 'Thought is required', 
+        trim: true,
         // 1 - 280 chars
     },
       createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
         // getter method to format timestamp on query 
+        get: createdAtVal => dateFormat(createdAtVal)
+
       },
       username: {
         type: String,
         required: 'Username is Required' 
       },
-      reactions: []
-});
+      reactions: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Reaction'
+        }
+      ]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      // prevents virtuals from creating duplicate of _id as `id`
+      id: false
+    }
+  );
+  
+  //Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+  // get total count of reactions on retrieval
+  ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reaction.reduce(
+      (total, reaction) => total + reaction.length + 1,
+      0
+    );
+  });
 
-//Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 
 const Thought = model('Thought', ThoughtSchema);
 
